79b0fa289db979b489e178d328a0fbd4182dc95f
sanpipop <sanpipop9@gmail.com>
2022-01-18 16:08:25 +0700

    new

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ const morgan = require('morgan')
 const path = require('path')
 
 const app = express()
-const port = 3000
+const PORT = process.env.PORT 
 
 app.use(morgan('combined'))
 app.use(express.static(path.join(__dirname,"/public/")))
 
 app.get("/", (req,res) =>{
 
-    res.send("Hello sanpipop Batriya kuyiter")
+    res.send("Hello sanpipop10100999 Batriya kuyiter")
 })
 
-app.listen(port, ()=>{
-    debug("Listening on port" + chalk.red(" : " + port))
+app.listen(PORT, ()=>{
+    debug("Listening on port" + chalk.red(" : " + PORT))
+    
 })
